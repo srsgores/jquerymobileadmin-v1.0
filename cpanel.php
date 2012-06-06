4b2b4270a375e575b38fc5847e34c91c27f6fce6
Sean Goresht <seangores@gmail.com>
2012-06-05 19:10:35 -0600

    First commit -- test
    A test

---
+++ cpanel.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @version � 2.6 April 10, 2012
+ * @author � �RocketTheme http://www.rockettheme.com
+ * @copyright Copyright (C) 2007 - 2012 RocketTheme, LLC
+ * @license � http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 only
+ */
+
+require_once(dirname(__FILE__).DS.'index.php');
